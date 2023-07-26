40f97dcfee102274c9b6ad69d57235e3c5c6017d
Mark Kraus <vindevccm@gmail.com>
2023-07-26 09:58:50 -0400

    poc

--- src/stacks/cicd.ts
+++ src/stacks/cicd.ts
@@ -6,6 +6,7 @@ import { AppStage, StageDefinitions, codestarConnectionArn, prodBranch } from '.
 import { AppConfig } from '../lib/getAppConfig';
 import { StackName } from '../main';
 import { Application } from './application';
+import { Cache, LocalCacheMode } from 'aws-cdk-lib/aws-codebuild';
 
 export class CICDStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps & AppConfig & { stackName: string; stageDefinitions: StageDefinitions; stack: StackName; }) {
@@ -33,7 +34,8 @@ export class CICDStack extends Stack {
       codeBuildDefaults: {
         buildEnvironment: {
           privileged: true,
-        }
+        },
+        cache: Cache.local(LocalCacheMode.SOURCE)
       },
       crossAccountKeys: true,
     });
