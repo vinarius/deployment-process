7bcf369278ffb97b8a88a14ab8d0082219b0ccc1
Mark Kraus <vindevccm@gmail.com>
2023-07-26 14:56:53 -0400

    poc

--- src/config.ts
+++ src/config.ts
@@ -10,16 +10,12 @@ export enum AppStage {
   cicd = 'cicd',
   dev = 'dev',
   test = 'test',
-  // staging = 'staging',
   prod = 'prod',
-  // prod2 = 'prod2',
-  // prod3 = 'prod3',
-  // postProd = 'postProd',
 }
 
 export const project = 'poc';
 export const prodBranch = 'main';
-export const codestarConnectionArn = 'arn:aws:codestar-connections:us-east-1:597119195378:connection/12e61d31-c78f-4f1d-9262-78e0e4fe0a52';
+export const codestarConnectionArn = '';
 
 export interface ApplicationDefinition extends AppConfig {
   stageDefinition: StageDefinition;
@@ -46,57 +42,29 @@ export const stageDefinitions: StageDefinitions = {
   [AppStage.cicd]: {
     name: AppStage.cicd,
     env: {
-      account: '597119195378',
-      region: 'us-east-2',
+      account: '',
+      region: '',
     },
   },
   [AppStage.dev]: {
     name: AppStage.dev,
     env: {
-      account: '476324220602',
-      region: 'us-east-2',
+      account: '',
+      region: '',
     },
   },
   [AppStage.test]: {
     name: AppStage.test,
     env: {
-      account: '580140692765',
-      region: 'us-east-2',
+      account: '',
+      region: '',
     },
   },
-  // [AppStage.staging]: {
-  //   name: AppStage.staging,
-  //   env: {
-  //     account: '',
-  //     region: 'us-east-2',
-  //   },
-  // },
   [AppStage.prod]: {
     name: AppStage.prod,
     env: {
-      account: '597119195378',
-      region: 'us-east-2',
+      account: '',
+      region: '',
     },
   },
-  // [AppStage.prod2]: {
-  //   name: AppStage.prod2,
-  //   env: {
-  //     account: '597119195378',
-  //     region: 'us-east-2',
-  //   },
-  // },
-  // [AppStage.prod3]: {
-  //   name: AppStage.prod3,
-  //   env: {
-  //     account: '597119195378',
-  //     region: 'us-east-2',
-  //   },
-  // },
-  // [AppStage.postProd]: {
-  //   name: AppStage.postProd,
-  //   env: {
-  //     account: '',
-  //     region: 'us-east-2',
-  //   },
-  // },
 };
